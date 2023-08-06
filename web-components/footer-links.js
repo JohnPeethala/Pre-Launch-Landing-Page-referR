ed3220d929929280c38678290ecbc2223ee4febb
John <74511150+JohnPeethala@users.noreply.github.com>
2023-08-06 17:26:44 +0530

    prettier changes

--- web-components/footer-links.js
+++ web-components/footer-links.js
@@ -1,30 +1,32 @@
 class FooterLinks extends HTMLElement {
-	connectedCallback() {
-		// Retrieve the links data from an attribute
-		let linksData = JSON.parse(this.getAttribute("links"));
-		if (!linksData) {
-			linksData = [
-				{
-					name: "Terms & Conditions",
-					link: "#",
-				},
-				{
-					name: "Privacy",
-					link: "#",
-				},
-			];
-		}
+  connectedCallback() {
+    // Retrieve the links data from an attribute
+    let linksData = JSON.parse(this.getAttribute("links"));
+    if (!linksData) {
+      linksData = [
+        {
+          name: "Terms & Conditions",
+          link: "#",
+        },
+        {
+          name: "Privacy",
+          link: "#",
+        },
+      ];
+    }
 
-		// Generate the markup for the links
-		const linksMarkup = linksData.map((link) => `<a href="${link.link}"><p>${link.name}</p></a>`).join("");
+    // Generate the markup for the links
+    const linksMarkup = linksData
+      .map((link) => `<a href="${link.link}"><p>${link.name}</p></a>`)
+      .join("");
 
-		// Set the innerHTML of the component
-		this.innerHTML = `
+    // Set the innerHTML of the component
+    this.innerHTML = `
         <div class="links">
           ${linksMarkup}
         </div>
       `;
-	}
+  }
 }
 
 // Define the custom element
