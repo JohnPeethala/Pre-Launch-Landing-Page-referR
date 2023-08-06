6eceeaf05ba03874c5e4fe490059d5867e74c9ac
John <74511150+JohnPeethala@users.noreply.github.com>
2023-08-06 16:45:46 +0530

    Initial commit

---
+++ web-components/footer-icons.js
@@ -0,0 +1,37 @@
+class FooterIcons extends HTMLElement {
+	connectedCallback() {
+		// Retrieve the icons data from an attribute
+		let iconsData = JSON.parse(this.getAttribute("icons"));
+		if (!iconsData) {
+			iconsData = [
+				{
+					alt: "linkedin",
+					src: "./public/footer-linked_in_dark.svg",
+					href: "#",
+				},
+				{
+					alt: "email",
+					src: "./public/footer-mail_dark.svg",
+					href: "#",
+				},
+			];
+		}
+
+		// Generate the markup for the icons
+		const iconsMarkup = iconsData
+			.map(
+				(icon) => `<a href="${icon.href}"><img width="25px" height="25px" alt="${icon.alt}" src="${icon.src}" /></a>`
+			)
+			.join("");
+
+		// Set the innerHTML of the component
+		this.innerHTML = `
+            <div class="icons">
+                ${iconsMarkup}
+            </div>
+        `;
+	}
+}
+
+// Define the custom element
+customElements.define("footer-icons", FooterIcons);
