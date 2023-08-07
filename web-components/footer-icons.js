f0e590c6055996e884331fd8def2facbb7c63844
John <74511150+JohnPeethala@users.noreply.github.com>
2023-08-08 01:08:31 +0530

    npx prettier

--- web-components/footer-icons.js
+++ web-components/footer-icons.js
@@ -1,61 +1,71 @@
 class FooterIcons extends HTMLElement {
-	connectedCallback() {
-		// default icons list
-		const defaultIcons = [
-			{
-				name: "linked_in",
-				url: "#",
-			},
-			{
-				name: "email",
-				url: "#",
-			},
-		];
+  connectedCallback() {
+    // default icons list
+    const defaultIcons = [
+      {
+        name: "linked_in",
+        url: "#",
+      },
+      {
+        name: "email",
+        url: "#",
+      },
+    ];
 
-		//create an empty array to show icons
-		let previewIcons = [];
+    //create an empty array to show icons
+    let previewIcons = [];
 
-		//check for addIcons attribute
-		if (this.getAttribute("addIcons")) {
-			let addIcons = his.getAttribute("addIcons").split(" ");
-			previewIcons = defaultIcons.filter((icon) => addIcons.includes(icon.name));
-		} else {
-			previewIcons = defaultIcons;
-		}
+    //check for addIcons attribute
+    if (this.getAttribute("addIcons")) {
+      let addIcons = his.getAttribute("addIcons").split(" ");
+      previewIcons = defaultIcons.filter((icon) =>
+        addIcons.includes(icon.name),
+      );
+    } else {
+      previewIcons = defaultIcons;
+    }
 
-		//check for removeIcons attribute
-		if (this.getAttribute("removeIcons")) {
-			let removeIcons = this.getAttribute("removeIcons").split(" ");
-			previewIcons = previewIcons.filter((icon) => !removeIcons.includes(icon.name));
-		}
+    //check for removeIcons attribute
+    if (this.getAttribute("removeIcons")) {
+      let removeIcons = this.getAttribute("removeIcons").split(" ");
+      previewIcons = previewIcons.filter(
+        (icon) => !removeIcons.includes(icon.name),
+      );
+    }
 
-		{
-			// the above whole process can be written simply
-			let previewIcons1 = this.getAttribute("addIcons") ? this.getAttribute("addIcons").split("") : defaultIcons;
+    {
+      // the above whole process can be written simply
+      let previewIcons1 = this.getAttribute("addIcons")
+        ? this.getAttribute("addIcons").split("")
+        : defaultIcons;
 
-			let removeIcons1 = this.getAttribute("removeIcons") ? this.getAttribute("removeIcons").split(" ") : [];
+      let removeIcons1 = this.getAttribute("removeIcons")
+        ? this.getAttribute("removeIcons").split(" ")
+        : [];
 
-			previewIcons1 = previewIcons1.filter((icon) => !removeIcons1.includes(icon.name));
-		}
+      previewIcons1 = previewIcons1.filter(
+        (icon) => !removeIcons1.includes(icon.name),
+      );
+    }
 
-		//retrive path
-		let path_extension = this.getAttribute("path") || "./public";
+    //retrive path
+    let path_extension = this.getAttribute("path") || "./public";
 
-		// Generate the markup for the icons
-		const iconsMarkup = previewIcons
-			.map(
-				(icon) =>
-					`<a href="${icon.url}"><img width="25px" height="25px" alt="${icon.name}" src="${path_extension}/footer-${icon.name}-dark.svg" /></a>`
-			)
-			.join("");
+    // Generate the markup for the icons
+    const iconsMarkup = previewIcons
+      .map(
+        (icon) =>
+          `<a href="${icon.url}"><img width="25px" height="25px" alt="${icon.name}" src="${path_extension}/footer-${icon.name}-dark.svg" /></a>`,
+      )
+      .join("");
 
-		// Set the innerHTML of the component
-		this.innerHTML = `
+    // Set the innerHTML of the component
+    this.innerHTML = `
             <div class="icons">
                 ${iconsMarkup}
             </div>
         `;
-	}
+  }
 }
 
 // Define the custom element
--- web-components/footer-links.js
+++ web-components/footer-links.js
@@ -1,53 +1,65 @@
 class FooterLinks extends HTMLElement {
-	connectedCallback() {
-		// default icons list
-		const defaultLinks = [
-			{
-				name: "Terms & Conditions",
-				url: "#",
-			},
-			{
-				name: "Privacy",
-				url: "./../ll",
-			},
-		];
+  connectedCallback() {
+    // default icons list
+    const defaultLinks = [
+      {
+        name: "Terms & Conditions",
+        url: "#",
+      },
+      {
+        name: "Privacy",
+        url: "./../ll",
+      },
+    ];
 
-		//create an empty array to show icons
-		let previewLinks = [];
+    //create an empty array to show icons
+    let previewLinks = [];
 
-		//check for addIcons attribute
-		if (this.getAttribute("addLinks")) {
-			let addIcons = his.getAttribute("addLinks").split(" ");
-			previewLinks = defaultLinks.filter((link) => addIcons.includes(link.name));
-		} else {
-			previewLinks = defaultLinks;
-		}
+    //check for addIcons attribute
+    if (this.getAttribute("addLinks")) {
+      let addIcons = his.getAttribute("addLinks").split(" ");
+      previewLinks = defaultLinks.filter((link) =>
+        addIcons.includes(link.name),
+      );
+    } else {
+      previewLinks = defaultLinks;
+    }
 
-		//check for removeIcons attribute
-		if (this.getAttribute("removeLinks")) {
-			let removeIcons = this.getAttribute("removeLinks").split(" ");
-			previewLinks = previewLinks.filter((icon) => !removeIcons.includes(icon.name));
-		}
+    //check for removeIcons attribute
+    if (this.getAttribute("removeLinks")) {
+      let removeIcons = this.getAttribute("removeLinks").split(" ");
+      previewLinks = previewLinks.filter(
+        (icon) => !removeIcons.includes(icon.name),
+      );
+    }
 
-		{
-			// the above whole process can be written simply
-			let previewLinks1 = this.getAttribute("addLinks") ? this.getAttribute("addLinks").split("") : defaultLinks;
+    {
+      // the above whole process can be written simply
+      let previewLinks1 = this.getAttribute("addLinks")
+        ? this.getAttribute("addLinks").split("")
+        : defaultLinks;
 
-			let removeLinks1 = this.getAttribute("removeIcons") ? this.getAttribute("removeIcons").split(" ") : [];
+      let removeLinks1 = this.getAttribute("removeIcons")
+        ? this.getAttribute("removeIcons").split(" ")
+        : [];
 
-			previewLinks1 = previewLinks1.filter((links) => !removeLinks1.includes(links.name));
-		}
+      previewLinks1 = previewLinks1.filter(
+        (links) => !removeLinks1.includes(links.name),
+      );
+    }
 
-		// Generate the markup for the icons
-		const linksMarkup = previewLinks.map((link) => `<a href="${link.url}"><p>${link.name}</p></a>`).join("");
+    // Generate the markup for the icons
+    const linksMarkup = previewLinks
+      .map((link) => `<a href="${link.url}"><p>${link.name}</p></a>`)
+      .join("");
 
-		// Set the innerHTML of the component
-		this.innerHTML = `
+    // Set the innerHTML of the component
+    this.innerHTML = `
         <div class="links">
           ${linksMarkup}
         </div>
       `;
-	}
+  }
 }
 
 // Define the custom element
